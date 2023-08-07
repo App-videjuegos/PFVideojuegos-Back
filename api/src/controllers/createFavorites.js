e3e04169aa2d3883f9d8dfa5d23f3abeb76601a2
cristianTabares86 <cristiantabares.iq@gmail.com>
2023-08-07 11:33:48 -0500

    FIX favoritos resuelto

--- api/src/controllers/createFavorites.js
+++ api/src/controllers/createFavorites.js
@@ -1,27 +1,44 @@
 const { Favorites, Videogame, Users } = require("../db");
 
 async function createFavorite({ videogameId, userId, isFav }) {
-    // Verificar si el videojuego existe en la base de datos
-    const videogame = await Videogame.findByPk(videogameId);
-    if (!videogame) {
-        throw new Error(`Videojuego con ID: ${videogameId}, no encontrado`);
+  // Verificar si el videojuego existe en la base de datos
+  const videogame = await Videogame.findByPk(videogameId);
+  if (!videogame) {
+    throw new Error(`Videojuego con ID: ${videogameId}, no encontrado`);
+  }
+  // Verificar si el usuario existe en la base de datos
+  const user = await Users.findByPk(userId);
+  if (!user) {
+    throw new Error(`Usuario con ID: ${userId}, no encontrado`);
+  }
+  // Si existe videojuego y usuario, crea un nuevo item con el favorito enviado
+  const existingFavorite = await Favorites.findOne({
+    where: {
+      userId: userId, // Reemplaza userId con el valor real del usuario que deseas buscar
+      videogameId: videogameId // Reemplaza videogameId con el valor real del videojuego que deseas buscar
     }
-    // Verificar si el usuario existe en la base de datos
-    const user = await Users.findByPk(userId);
-    if (!user) {
-      throw new Error(`Usuario con ID: ${userId}, no encontrado`);
-    }
-    // Si existe videojuego y usuario, crea un nuevo item con el favorito enviado
-  try {
-    let favorite = await Favorites.create({
+  });
+
+  if (existingFavorite) {
+    existingFavorite.isFav = isFav;
+    await Favorites.update(existingFavorite, {
+      where: {
+        id: existingFavorite.id
+      },
+  })
+  return existingFavorite
+  } else {
+    try {
+      let favorite = await Favorites.create({
         videogameId,
         userId,
         isFav,
-    })
-    return favorite
-  } catch (error) {
-    console.log(error);
-    return new Error ('Ha ocurrido un error al agregar el favorito'); 
+      })
+      return favorite
+    } catch (error) {
+      console.log(error);
+      return new Error('Ha ocurrido un error al agregar el favorito');
+    }
   }
 }
 
